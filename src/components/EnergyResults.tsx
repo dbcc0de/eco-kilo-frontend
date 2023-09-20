563aafd04ef08beed1d50f8e9456c5936b6d62ab
Conor Hutton <conor03@gmail.com>
2023-09-20 17:05:57 -0400

    energyresults

--- src/components/EnergyResults.tsx
+++ src/components/EnergyResults.tsx
@@ -9,6 +9,8 @@ interface Props {
 }
 
 const EnergyResults = ({ home }: Props) => {
+  let [offPeakCostCounter, setOffPeakCostCounter] = useState(0);
+  let [peakCostCounter, setPeakCostCounter] = useState(0);
   const [utilityResponse, setUtilityResponse] =
     useState<UtilityResponse | null>(null);
   useEffect(() => {
@@ -24,17 +26,29 @@ const EnergyResults = ({ home }: Props) => {
 
   const peakAndOffPeakCalculation = (home: Home): void => {
     if (home.appliances) {
+      setPeakCostCounter(0);
+      setOffPeakCostCounter(0);
       home.appliances.map((item) => {
         if (item.endTime <= 19 && item.startTime >= 15) {
           console.log("This appliance is only using peak hours.");
-          const kwhPerDay = item.kwh * (item.endTime - item.startTime);
-          const costPerDay =
+          const kwhPerDay: number = item.kwh * (item.endTime - item.startTime);
+          const costPerDay: number =
             Number(
               kwhPerDay * (utilityResponse?.outputs.residential || 0.1611)
             ) * 1.64;
-          console.log(costPerDay);
+          setPeakCostCounter((prev) => (prev += costPerDay));
+          console.log(peakCostCounter);
         } else if (item.endTime > 15 && item.startTime < 19) {
           console.log("Uses peak and off peak hours");
+          //  TV starts at 1 and goes until 10
+          //  1-3 is off peak, 3-7 is peak, 7-10 is off peak
+          // TV begins at 13, ends at 22
+          // 15 - 19 peak hours
+          // 15 - 13 = 2 hours before peak
+          //   22 - 19 = 3 hours after peak
+          //  peak hours = end time - start time - hours before peak - hours after peak
+
+          // setpeakCostCounter((peakCostCounter += costPerDay));
         } else if (
           item.endTime < 15 ||
           item.endTime > 19 ||
@@ -42,6 +56,49 @@ const EnergyResults = ({ home }: Props) => {
           item.startTime > 19
         ) {
           console.log("This appliance uses off peak hours");
+          const kwhPerDay: number = item.kwh * (item.endTime - item.startTime);
+          const costPerDay: number = Number(
+            kwhPerDay * (utilityResponse?.outputs.residential || 0.1611)
+          );
+          setOffPeakCostCounter((prev) => (prev += costPerDay));
+        }
+      });
+    }
+  };
+
+  const calculateAllHours = (home: Home): void => {
+    const peakStart = 15;
+    const peakEnd = 19;
+    if (home.appliances) {
+      home.appliances.map((item) => {
+        let beforePeak = null;
+        if (item.startTime < peakStart) {
+          if (item.endTime <= peakStart) {
+            beforePeak = item.endTime - item.startTime;
+          } else {
+            beforePeak = peakStart - item.startTime;
+          }
+        }
+        let duringPeak: number | null = null;
+        if (item.startTime <= peakEnd && item.endTime >= peakStart) {
+          if (item.startTime >= peakStart && item.endTime <= peakEnd) {
+            duringPeak = item.endTime - item.startTime;
+            const peakCost =
+              Number(duringPeak) *
+              item.kwh *
+              (utilityResponse?.outputs.residential || 0.1611);
+            setPeakCostCounter((prev) => (prev += Number(peakCost)));
+          } else {
+            duringPeak = peakEnd - item.startTime;
+          }
+        }
+        let afterPeak = null;
+        if (item.endTime > peakEnd) {
+          if (item.startTime <= peakEnd) {
+            afterPeak = item.endTime - peakEnd;
+          } else {
+            afterPeak = item.endTime - item.startTime;
+          }
         }
       });
     }
@@ -72,29 +129,14 @@ const EnergyResults = ({ home }: Props) => {
         Your utility rate is: {utilityResponse?.outputs.residential} cents per
         kilowatt hour.{" "}
       </p>
+      <p>Peak rate costs: ${peakCostCounter.toFixed(2)} </p>
+      <p>Off peak rate costs: ${offPeakCostCounter.toFixed(2)} </p>
+      <p>Total costs: ${(offPeakCostCounter + peakCostCounter).toFixed(2)}</p>
       {/* add utility rate */}
       {/* add average utility rate and bill */}
       {/* suggest improvements  */}
       <button onClick={() => kwhSumAllAppliances(home)}>Calculate</button>
-      <button onClick={() => peakAndOffPeakCalculation(home)}>
-        Check For Peak
-      </button>
-
-      {home.appliances ? (
-        <>
-          {" "}
-          <p>{home.appliances[0].kwh} kwh</p>
-          <p>{home.appliances[0].startTime}</p>
-          <p>{home.appliances[0].endTime}</p>
-          <p>
-            Result of Appliance 1:{"Total kWhs "}
-            {(home.appliances[0].endTime - home.appliances[0].startTime) *
-              home.appliances[0].kwh}
-          </p>
-        </>
-      ) : (
-        <p>Get rekt</p>
-      )}
+      <button onClick={() => calculateAllHours(home)}>Check For Peak</button>
     </div>
   );
 };
