91f22cc5ba43437517542a2e393dae446102ae8d
Conor Hutton <conor03@gmail.com>
2023-09-25 16:54:04 -0400

    educationpageformatting

--- src/components/FaqCard.tsx
+++ src/components/FaqCard.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./FaqCard.css";
 import Faq from "../models/Faq";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface Props {
   faq: Faq;
@@ -10,10 +12,19 @@ const FaqCard = ({ faq }: Props) => {
   const [displayFAQ, setDisplayFAQ] = useState(false);
   return (
     <li className="FaqCard">
-      <button onClick={() => setDisplayFAQ((prev) => !prev)}>X</button>
-      <div>
-        <p> {faq.question}</p>
-        {displayFAQ && <p>{faq.answer}</p>}
+      <div id="CardContainer">
+        <div>
+          <button onClick={() => setDisplayFAQ((prev) => !prev)}>
+            {" "}
+            <FontAwesomeIcon icon={faAngleDown} />
+          </button>
+        </div>
+        <div>
+          {/* <p id="appliancesLabel"> */}
+
+          <p id="faqQuestion"> {faq.question}</p>
+          {displayFAQ && <p>{faq.answer}</p>}
+        </div>
       </div>
     </li>
   );
